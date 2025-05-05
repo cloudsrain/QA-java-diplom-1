379e81bd1e8cbf076d24baa583e8c157ef230757
Andrey Kan <andreyevdaaa@yandex.ru>
2025-05-05 14:22:11 +0600

    test:Написал тесты на добавление и удаление игредиентов в бургер в тестовом классе "BurgerTest"

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -24,6 +24,26 @@ public class BurgerTest {
         assertEquals("Булка установлена некорректно", bun, burger.bun);
     }
 
+    @Test
+    public void runAddIngredientTest(){
+        Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "hot sauce", 100);
+        Burger burger = new Burger();
 
+        burger.addIngredient(ingredient);
+
+        assertEquals("В списке больше одного ингредиента", 1, burger.ingredients.size());
+        assertEquals("В список добавился не тот ингредиент", ingredient, burger.ingredients.get(0));
+    }
+
+    @Test
+    public void runRemoveIngredientTest(){
+        Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "hot sauce", 100);
+        Burger burger = new Burger();
+
+        burger.addIngredient(ingredient);
+
+        burger.removeIngredient(0);
+        assertTrue("Ингредиент не убрался", burger.ingredients.isEmpty());
+    }
 
 }
