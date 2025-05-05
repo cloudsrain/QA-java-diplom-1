745d37c958d1111d59233884b2bc19cda1bb3cd3
Andrey Kan <andreyevdaaa@yandex.ru>
2025-05-05 14:20:38 +0600

    test:Написал тест на установку булки в бургер в тестовом классе "BurgerTest"

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -1,2 +1,29 @@
+import praktikum.Bun;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+import praktikum.Burger;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.Mock;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
 public class BurgerTest {
+
+
+
+    @Test
+    public void runSetBunsTest() {
+        Bun bun = new Bun("black bun", 100);
+        Burger burger = new Burger();
+
+        burger.setBuns(bun);
+
+        assertEquals("Булка установлена некорректно", bun, burger.bun);
+    }
+
+
+
 }
