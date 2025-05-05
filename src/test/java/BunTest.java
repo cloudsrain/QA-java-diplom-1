2ecdacb6189f14ca78f05afea866527618ed860e
Andrey Kan <andreyevdaaa@yandex.ru>
2025-05-05 14:18:11 +0600

    test: Написал парметризованный тест на класс "Bun"

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,2 +1,46 @@
+import praktikum.Bun;
+
+import org.junit.runner.RunWith;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
 public class BunTest {
+
+    private final String name;
+    private final float price;
+
+    private String actualName;
+    private float actualPrice;
+
+    public BunTest(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters(name ="Тест {index}: {0}, {1}")
+    public static Object[][] getOrder() {
+        return new Object[][]{
+                {"black bun", 100},
+                {"white bun", 200},
+                {"red bun", 300},
+
+        };
+    }
+
+    @Before
+    public void bunCreation(){
+        Bun bun = new Bun(name, price);
+        actualName = bun.getName();
+        actualPrice = bun.getPrice();
+    }
+
+    @Test
+    public void runBunTest(){
+        assertEquals("Ожидалось другое название булочки", name, actualName);
+        assertEquals("Ожидалось другая цена булочки", price, actualPrice, 0.001f);
+    }
+
 }
